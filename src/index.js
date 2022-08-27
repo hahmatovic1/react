c0afd7b56fe53180019f74c7c3c077b8823e23e2
hahmatovic1 <hahmatovic1@etf.unsa.ba>
2022-08-27 08:27:48 +0000

    add h1 and list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+    <div>
+        <h1>Favorite plants</h1>
+        <ul>
+            <li>dandelions</li>
+            <li>lemon tree</li>
+            <li>orange tree</li>
+        </ul>
+    </div>,
+    document.getElementById("root")
 );

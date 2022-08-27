d2baa972bd4cdf4764c27474d32e658109b769e0
hahmatovic1 <hahmatovic1@etf.unsa.ba>
2022-08-27 08:34:25 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Hadija";
+const luckyNumber = 3;
+
 ReactDOM.render(
     <div>
-        <h1>Favorite plants</h1>
-        <ul>
-            <li>dandelions</li>
-            <li>lemon tree</li>
-            <li>orange tree</li>
-        </ul>
+        <h1>Hello {name}!</h1>
+        <p>your lucky number is {luckyNumber}</p>      
     </div>,
     document.getElementById("root")
 );
